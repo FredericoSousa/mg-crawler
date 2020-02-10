00f2de52ba7c94009a25aa3c46aa0f4dbdfb0e01
Frederico Sousa <frederico.sousa@squadra.com.br>
2020-02-10 18:30:57 -0200

    heroku port

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-require("./routes")(app);
+require('./routes')(app);
 
-const { port = 3000 } = require("./config").app;
-app.listen(port, console.log(`Server listen on port ${port}`));
+const { port = 3000 } = require('./config').app;
+app.listen(port || process.env.PORT, console.log(`Server listen on port ${port}`));
